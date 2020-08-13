9179d3ab1bf8c50b9ae7f23c3e7a296a76ba5678
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2020-08-14 00:23:03 +0700

    add services page, update purgecss config

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  content: ["src/index.html"],
-  css: ["src/css/main.css"],
+  content: [
+    "src/index.html",
+    "src/about/index.html",
+    "src/services/index.html",
+  ],
+  css: ["src/assets/css/main.css"],
 };
