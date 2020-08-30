895e852ea9d0083afd3b4f94d8d7cf8f54f69ece
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2020-08-30 22:11:44 +0700

    new service page

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,6 +3,10 @@ module.exports = {
     "src/index.html",
     "src/about/index.html",
     "src/services/index.html",
+    "src/services/pembuatan-website/index.html",
+    "src/services/digital-marketing/index.html",
+    "src/services/desain-grafis/index.html",
+    "src/services/implementasi-erp/index.html",
   ],
   css: ["src/assets/css/main.css"],
 };
