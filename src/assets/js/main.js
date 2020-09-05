ce8b25002a9cdfb46b37834e32588ea08bef2e2e
rochimfn <rochim.noviyan@gmail.com>
2020-09-05 21:00:26 +0700

    new desain grafis page, add seo meta tag, fix some layout and js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+
+const openMenu = document.querySelector("#openMenu");
+openMenu.addEventListener("click", () => {
+  let menu = document.querySelector(".menu");
+  menu.classList.remove("d-none");
+  menu.classList.add("animate__fadeIn");
+});
+const closeMenu = document.querySelector("#closeMenu");
+closeMenu.addEventListener("click", () => {
+  let menu = document.querySelector(".menu");
+  menu.classList.add("d-none");
+  menu.classList.remove("animate__fadeIn");
+});
