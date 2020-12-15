8bc4474a2780bb894bd1227540f33c9816b78dad
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2020-12-15 22:10:38 +0700

    Updates

--- main.cea5deef.js
+++ main.cea5deef.js
@@ -158,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42151" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43487" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
